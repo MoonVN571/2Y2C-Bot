adfae6ba7d138891b07fde9e6cea4f8417f59981
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2021-02-21 13:39:47 +0700

    Delete ingame-commands directory

--- ingame-commands/help.js
+++
@@ -1,5 +0,0 @@
-exports.run = (bot, username, logger) => {
-    if(logger.startsWith("!help")) {
-        bot.whisper(username, 'Một số lệnh bạn có thể sử dụng: !discord, !buykit,.. Xem đầy đủ lệnh tại: dicord.gg/yrNvvkqp6w')
-    }
-}
--- ingame-commands/tps.js
+++
@@ -1,5 +0,0 @@
-exports.run = (bot, name, logger) => {
-    if(logger.startsWith("!tps")) {
-        bot.whisper(username, `Server TPS: ${bot.getTps()}`)
-    }
-}
