6657e7a46c261f0eac343d759b66fe439c91ab4e
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2021-04-14 19:53:16 +0700

    Release 1.6

--- events-ingame/login.js
+++ events-ingame/login.js
@@ -118,7 +118,7 @@ module.exports = (bot, client) => {
                 setInterval(() => {
                     if (guild[0]) {
                         const line = guild.pop()
-                        const data = new bot.Scriptdb(`./data/guilds/setup-${line}.json`);
+                        const data = new Scriptdb(`./data/guilds/setup-${line}.json`);
                         const checkdata = data.get('livechat');
 
                         if(checkdata == undefined || guild == undefined) return;
