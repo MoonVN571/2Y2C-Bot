590647cb58facbe54b2c7cb725137e813543b86d
MoonVNN <tuando000.dthap@gmai.com>
2021-05-09 13:05:30 +0700

    sửa không hiển thị status

--- commands/status.js
+++ commands/status.js
@@ -10,27 +10,30 @@ module.exports = {
     aliases: ['uptime', 'tps', 'q', 'queue', 'que', 'prio', 'prioqueue', 'normalqueue'],
 
     async execute(client, message, args) {
-        var data = new Scriptdb(`./data.json`);
+        var data = new Scriptdb(`../data.json`);
 
         var queue = data.get('queue');
         var prio = data.get('prio');
 
-        var tab = data.get('tab-content').toString();
+        var tab = data.get('tab-content');
+
+        if(tab == undefined) return message.channel.send("Bot chưa kết nối đến server hoặc server không hoạt động.");
+
 		var uptime = tab.split(' - ')[3].split(" | ")[0].split("restart từ")[1].split("trước")[0];
 		var tps = tab.split(' ')[1];
 		var players = tab.split(' ')[4];
 		var ping = tab.split(" - ")[2].split(" ping")[0];
 		var timepassed  = tab.split(" | ")[1];
 
-        var user = client.users.cache.find(user => user.id === "425599739837284362")
-        if(tps == "tps" || tps == " tps" || tps == "ping" || tps == " ping" || tps == "players" || tps == " players")
-         
+        var user = client.users.cache.find(user => user.id === "425599739837284362");
+
+        if(tps == "tps" || ping == "ping"|| players == "players")
                     return message.channel.send(`Lỗi, thử lại sau! Hãy báo cáo lỗi này với **${user.username}#${user.discriminator}`)
 
         if(queue == undefined || prio == undefined) return message.channel.send("Không tìm thấy dữ liệu.");
         
 		var embed = new Discord.MessageEmbed()
-                .setAuthor('2Y2C VIETNAM','https://cdn.discordapp.com/attachments/795842485133246514/821669964673974282/communityIcon_14otnpwidot51.png')
+                .setAuthor('2Y2C','https://cdn.discordapp.com/attachments/795842485133246514/821669964673974282/communityIcon_14otnpwidot51.png')
                 .addFields(
                     {
                         name: 'Server Uptime',
@@ -54,12 +57,12 @@ module.exports = {
                     },
                     {
                         name: 'Bot Ping',
-                        value: ping + "ms",
+                        value: ping + " ms",
                         inline: true
                     },
                     {
                         name: 'Hàng chờ',
-                        value: "Bình thường: " + queue.toString().split(" | ")[0] + " - Ưu tiên: " + prio.toString().split(" | ")[0],
+                        value: "Bình thường: " + queue.split(" | ")[0] + " - Ưu tiên: " + prio.split(" | ")[0],
                         inline: true
                     }
                     )
