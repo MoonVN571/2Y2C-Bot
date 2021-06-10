b1cf104e63227132749cf1da4a1acfc566100bb2
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2021-06-10 19:13:56 +0700

    design

--- index.js
+++ index.js
@@ -33,19 +33,25 @@ const log = require('./log');
 var abc = require("./api");
 var api = new abc();
 
-// env file
+// env file need to hide
 require('dotenv').config();
+
 const config = {
 	token: process.env.TOKEN,
-	ip: process.env.IP
+	username: process.env.ign,
+	devUsername: process.env.ignDev,
+	footer: "moonbot 2021",
+	prefix:  "$",
+	devPrefix: "dev$",
+	channel: "795135669868822528",
+	devChannel: "802454010400604161",
+	ingamePrefix: "!",
+	ingamePrefixDev: "!",
+	chatColor: "0x979797",
+	chatColorHighlight: "0x2EA711",
+	botEmbedColor: "0x000DFF",
+	author: "425599739837284362"
 };
-
-// Var
-const footer = "moonbot 2021";
-client.footer = footer;
-
-const prefix = "$";
-client.prefix = prefix;
 
 // Value
 let cooldown = new Set();
@@ -53,22 +59,26 @@ let antiSpam = new Set();
 
 const dev = true;
 
-if (dev) prefix = "dev$";
-
+var prefix = config.prefix;
 var defaultChannel;
-var devuser = "mo0nbot";
+var usernameBot;
 
 if (dev) {
-	defaultChannel = '802454010400604161';
-	devuser = "mo0nbot2";
+	defaultChannel = config.devChannel;
+	usernameBot = config.devUsername;
+	prefix = config.devPrefix;
 } else {
-	defaultChannel = '795135669868822528';
-	devuser = "mo0nbot";
+	defaultChannel = config.channel;
+	usernameBot = config.username;
 }
 
-/*
- *				READY
- */
+const footer = config.footer;
+client.footer = footer;
+
+const prefixSet = config.prefix;
+client.prefix = prefixSet;
+
+
 client.on('ready', () => {
 	client.user.setActivity("RESTARTING", { type: 'PLAYING' });
 	
@@ -81,12 +91,9 @@ client.on('ready', () => {
 	console.log('------------------------');
 
 	console.log('Bot started!');
-
 	console.log('Developer: ' + dev.toString().replace(/t/, "T").replace(/f/, "F"));
 	
-	log("Bot ready");
-
-	createBot();
+	log("Ready to create bot");
 
 	const data = new Scriptdb(`./data.json`);
 
@@ -96,126 +103,99 @@ client.on('ready', () => {
 	data.set('tab-content', null);
 	data.set('uptime', null);
 	data.set('players', null);
+
+	createBot();
 });
 
-async function sendMessage(channel, content) {
-	var onChannel = client.channels.cache.get(channel);
-	
-	if(!onChannel) return console.log("Không thấy kênh " + channel);
 
-	await onChannel.send(content);
-}
 
-/*
- *				START_BOT
- */
+
+
 function createBot() {
 	console.log('------------------------');
-	log("Creating bot");
+	log("Bot function started");
 	
 	const bot = mineflayer.createBot({
-		host: config.ip,
+		host: "2y2c.org",
 		port: 25565,
-		username: devuser,
-		version: "1.16.4"
+		username: usernameBot,
+		version: "1.16.5"
 	});
 
     bot.loadPlugin(tpsPlugin);
 	bot.loadPlugin(pathfinder);
-
-	const tpsEvent = require('./events-ingame/server-tps.js');
-	const autoEvent = require('./events-ingame/auto.js');
-	const msgEvent = require(`./events-ingame/msg.js`);
-	const joinedEvent = require(`./events-ingame/join.js`);
-	const leftEvent = require(`./events-ingame/left.js`);
-	const queueEvent = require(`./events-ingame/queue-tab.js`);
-	const mainEvent = require(`./events-ingame/main-tab.js`);
-	const ServerStatusEvent = require(`./events-ingame/status-tab.js`);
-	const verifyEvent = require('./events-ingame/verify.js');
-	const playtimeEvent = require('./events-ingame/playtime.js');
-	const JoinedServerEvent = require('./events-ingame/login.js');
-	const kickedEvent = require('./events-ingame/kicked.js');
-	const endedEvent = require('./events-ingame/ended.js');
 	
-	var color = "0x979797";
-	var color2 = "0x979797";
-
-	var lobby = true;
-
-	// New
-	var joined = false;
-	var countPlayers = 0;
+	var color = config.chatColor; // single channel
+	var color2 = config.chatColor; // multi channel
 
 	// Import
 	bot.defaultChannel = defaultChannel; // Kenh mat dinh cua chat
 	bot.dev = dev; // developer mode
+
+	var lobby = true; // Bot in queue
 	bot.lobby = lobby;
+
+	var joined = false; // check bot is joined
 	bot.joined = joined;
+
+	var countPlayers = 0; // Join spam fix
 	bot.countPlayers = countPlayers;
 	
+	const verifyEvent = require('./events-ingame/verify.js');
 	bot.on('windowOpen', verifyEvent.bind(null, bot));
 
+	const autoEvent = require('./events-ingame/auto.js');
+	const JoinedServerEvent = require('./events-ingame/login.js');
+	const playtimeEvent = require('./events-ingame/playtime.js');
 	bot.once('login', autoEvent.bind(null, bot, client));
 	bot.once('login', JoinedServerEvent.bind(null, bot, client));
 	bot.once('login', playtimeEvent.bind(null, bot));
+
+	const tpsEvent = require('./events-ingame/server-tps.js');
 	bot.once('login', tpsEvent.bind(null, client));
 
 	bot.on('message', msg => {
 		if (!(msg.toString().startsWith("<"))) return;
-	
 		var username = msg.toString().split(" ")[0].split("<")[1].split(">")[0];
-	
-		if(username.startsWith("[")) {
-			username = username.split("]")[1];
-		}
-	
+		if(username.startsWith("[")) username = username.split("]")[1];
 		logger = msg.toString().substr(msg.toString().split(" ")[0].length + 1);
 	
-		if (logger.startsWith(">")) {
-			color2 = "2EA711";
-		}
+		if (logger.startsWith(">")) color2 = config.chatColorHighlight;
 	
-		var bp;
-		if (bot.dev) {
-			bp = "!!";
-		} else {
-			bp = "!";   
-		}
+		var bp = config.ingamePrefix;
+		if (dev) bp = config.ingamePrefixDev;
 		
-		var chat2 = new Discord.MessageEmbed()
+		var chat = new Discord.MessageEmbed()
 						.setDescription(`**<${api.removeFormat(username)}>** ${api.removeFormat(logger)}`)
 						.setColor(color2);
 	
+		try {
+			client.channels.cache.get(bot.defaultChannel).send(chat);
+			color2 = "0x797979";
+		} catch (e) {}
+
+		// check if message start with > and change color
 		var setLogger = `**<${api.removeFormat(username)}>** ${api.removeFormat(logger)}`;
-		
-		
+
 		client.guilds.cache.forEach((guild) => {
 			const data = new Scriptdb(`./data/guilds/setup-${guild.id}.json`);
 			const checkdata = data.get('livechat');
 	
 			if(checkdata == undefined || guild == undefined) return;
 					
-			if(setLogger.split(" ")[1].startsWith(">")) {
-				color = "2EA711";
-			}
+			if(setLogger.split(" ")[1].startsWith(">")) color = config.chatColorHighlight;
 
-			var chat = new Discord.MessageEmbed()
+			let embedChat = new Discord.MessageEmbed()
 						.setDescription(setLogger)
 						.setColor(color);
 
 			if(dev) return;
-			if(chat == undefined) return;
 
 			try {
-				client.channels.cache.get(checkdata).send(chat)
-				color = "0x797979";
+				client.channels.cache.get(checkdata).send(embedChat);
+				color = config.chatColor;
 			} catch(e) {}
 		});
-		
-		if(chat2 !== undefined) {
-			client.channels.cache.get(bot.defaultChannel).send(chat2);
-			color2 = "0x797979";
-		}
 	
 		saveMsgsData(username, logger);
 		function saveMsgsData(username, logger) {
@@ -223,6 +203,7 @@ function createBot() {
 			let messages = new Scriptdb(`./data/quotes/${username}.json`);
 			let msgs = messages.get("messages")
 			let times = messages.get("times")
+
 			if(msgs == undefined) {
 				messages.set("messages", logger)
 				messages.set("times", Date.now())
@@ -290,15 +271,23 @@ function createBot() {
 		}, 1* 1000);
 	});
 
+	const msgEvent = require(`./events-ingame/msg.js`);
 	bot.on("message", msgEvent.bind(null, bot, client));
 
+	const joinedEvent = require(`./events-ingame/join.js`);
+	const leftEvent = require(`./events-ingame/left.js`);
 	bot.on("playerJoined", joinedEvent.bind(null, bot, client));
 	bot.on("playerLeft", leftEvent.bind(null, bot, client));
 
+	const ServerStatusEvent = require(`./events-ingame/status-tab.js`);
+	const mainEvent = require(`./events-ingame/main-tab.js`);
+	const queueEvent = require(`./events-ingame/queue-tab.js`);
 	bot._client.on("playerlist_header", ServerStatusEvent.bind(null, bot));
 	bot._client.on("playerlist_header", mainEvent.bind(null, bot, client));
 	bot._client.on("playerlist_header", queueEvent.bind(null, bot, client));
 
+	const endedEvent = require('./events-ingame/ended.js');
+	const kickedEvent = require('./events-ingame/kicked.js');
 	bot.on('kicked', kickedEvent.bind(null, bot, client));
 	bot.on('end', endedEvent.bind(null, bot, client));
 
@@ -333,7 +322,7 @@ function createBot() {
 
 		if (msg.channel.id == defaultChannel) {
 			if (msg.content.startsWith(">")) return;
-			if (msg.content.startsWith(prefix)) return;
+			if (msg.content.startsWith(prefixSet)) return;
 
 			var content = msg.content;
 			
@@ -401,13 +390,13 @@ for (const file of cmdss) {
 }
 
 client.on("message", async message => {
-	if(message.author.bot|| !message.content.startsWith(prefix) || message.author == client.user || message.channel.type == "dm") return;
+	if(message.author.bot|| !message.content.startsWith(prefixSet) || message.author == client.user || message.channel.type == "dm") return;
 
 	if(dev && message.guild.id !== "794912016237985802") return message.channel.send("Lệnh đã bị tắt tại nhóm này.");
 	
-	if(dev && message.author.id !== "425599739837284362") return message.channel.send("Chỉ developer mới có thể sử dụng bot này.");
+	if(dev && message.author.id !== author) return message.channel.send("Chỉ developer mới có thể sử dụng bot này.");
 
-    const args = message.content.slice(prefix.length).split(/ +/);
+    const args = message.content.slice(prefixSet.length).split(/ +/);
     const cmdName = args.shift().toLowerCase();
 
     const cmd = client.commandss.get(cmdName)
@@ -418,12 +407,12 @@ client.on("message", async message => {
 							.setDescription('Bạn phải là nhà phát triển để sử dụng lệnh này.')
 							.setColor('0xC51515');
 
-		if(message.author.id !== "425599739837284362")
-			return message.channel.send(noPerm).then(msg => {
-				msg.delete({ timeout: 10000 });
-			});
+		if(message.author.id !== config.author)
+		return message.channel.send(noPerm).then(msg => {
+			msg.delete({ timeout: 10000 });
+		});
 
-			delete require.cache[require.resolve(`./commands/${args[0]}.js`)];
+		delete require.cache[require.resolve(`./commands/${args[0]}.js`)];
 
 		const cmd = require(`./commands/${args[0]}`);
 		client.commandss.set(cmd.name, cmd);
@@ -441,12 +430,12 @@ client.on("message", async message => {
 							.setDescription('Bạn phải là nhà phát triển để sử dụng lệnh này.')
 							.setColor('0xC51515');
 
-		if(message.author.id !== "425599739837284362")
-			return message.channel.send(noPerm).then(msg => {
-				msg.delete({ timeout: 10000 });
-			});
+		if(message.author.id !== config.author)
+		return message.channel.send(noPerm).then(msg => {
+			msg.delete({ timeout: 10000 });
+		});
 
-			delete require.cache[require.resolve(`./ingame-commands/${args[0]}.js`)];
+		delete require.cache[require.resolve(`./ingame-commands/${args[0]}.js`)];
 
 		const cmd = require(`./ingame-commands/${args[0]}`);
 		client.commands.set(cmd.name, cmd);
@@ -459,7 +448,6 @@ client.on("message", async message => {
 		message.channel.send(successful);
 	}
 
-
     if(!cmd) return;
 	
 	client.userNotFound = new Discord.MessageEmbed()
@@ -468,7 +456,7 @@ client.on("message", async message => {
 	
 	client.footer = footer;
 	client.dev = dev;
-	client.color = "0x000DFF";
+	client.color = config.botEmbedColor;
 	client.prefix = prefix;
 
 	client.ping = client.ws.ping;
@@ -476,13 +464,14 @@ client.on("message", async message => {
     try{
         cmd.execute(client, message, args);
     }catch(err) {
-		message.author.send(err.toString()).then(() => {
-			message.author.send("Hãy báo cáo lỗi này cho admin nếu bạn nghĩ đây là do bot.");
-		})
-		console.log(cmdName);
         console.log(err);
+		console.log(err.toString());
     }
 });
 
 client.login(config.token).catch(err => console.log(err));
-client.on("error", (e) => { console.error(e) });
+client.on("error", (e) => {
+	console.error(e);
+	var error = err.toString();
+	
+});
